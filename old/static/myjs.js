5f755e3ba074a48c21c697b5959071777d0ba55e
Miguel <miguelbuzz100@gmail.com>
2024-06-23 17:21:56 +0200

    change

--- old/static/myjs.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $(".exercisesolution").hide();
-    $(".exercisetoggle").click(function(){
-    console.log("hi");
-    $(this).next(".exercisesolution").toggle(400);
-    });
-});
